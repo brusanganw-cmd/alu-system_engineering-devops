5e3cda6451227eeacb5e0f36a8b0ee119c0fd929
brusanganw-cmd <b.rusanganw@alustudent.com>
2026-04-04 23:36:54 +0200

    strace

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,5 @@
-# This Puppet manifest fixes WordPress file permissions so Apache can serve files without 500 errors
-file { '/var/www/html':
-  ensure  => directory,
-  recurse => true,
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0755',
+# Fix WordPress 500 error by correcting .phpp typo to .php
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
 }
